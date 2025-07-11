2a7fe47d799d29db31e4fdadd9d7651826ee6533
Afif Ali Saadman <afifsaadman2013@gmail.com>
2025-07-11 16:39:38 +0600

    Finally, GIT freed me

---
+++ upload_code.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/sugma.ssh
+git add .
+echo "Enter your commit message: "
+read commit_message
+git commit -m $commit_message
+clear
+echo "Committing updates to github"
+git pull origin main
+git push origin main
